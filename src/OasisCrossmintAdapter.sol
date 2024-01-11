fb5600d219881d48422f092b6a590186b4d2ca64
Sweets Sweetman <sweetmantech@gmail.com>
2024-01-11 13:26:53 -0300

    V0.0.0

--- src/OasisCrossmintAdapter.sol
+++ src/OasisCrossmintAdapter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.22;
 
 /*
 ███████╗██╗    ██╗███████╗███████╗████████╗███╗   ███╗ █████╗ ███╗   ██╗   ███████╗████████╗██╗  ██╗
@@ -9,24 +9,21 @@ pragma solidity ^0.8.10;
 ███████║╚███╔███╔╝███████╗███████╗   ██║   ██║ ╚═╝ ██║██║  ██║██║ ╚████║██╗███████╗   ██║   ██║  ██║
 ╚══════╝ ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝                                                                                              
 */
-import {IERC721Drop} from "./interfaces/IERC721Drop.sol";
-import {IERC721A} from "@ERC721A/contracts/IERC721A.sol";
-import {IZora1155} from "./interfaces/IZora1155.sol";
+import {Multicall3} from "@multicall/Multicall3.sol";
 
 contract OasisCrossmintAdapter {
-    /// @notice mint target ERC1155Drops
-    /// @param _target ERC721Drop contract address
-    /// @param _quantity number of tokens
-    /// @param _to recipient of tokens
-    function purchase(
+    Multicall3 public multicallContract;
+
+    constructor(address _multicallAddress) {
+        multicallContract = Multicall3(_multicallAddress);
+    }
+
+    function checkout(
         address _target,
         uint256 _quantity,
-        address _to
+        address _to,
+        Multicall3.Call3Value[] calldata cart
     ) public payable {
-        IERC721Drop erc721 = IERC721Drop(_target);
-        uint256 start = erc721.purchase{value: msg.value}(_quantity) + 1;
-        for (uint256 i = start; i < start + _quantity; i++) {
-            IERC721A(_target).transferFrom(address(this), _to, i);
-        }
+        multicallContract.aggregate3Value{value: msg.value}(cart);
     }
 }
